53a376b1225a6b11d1d6893220680c8324f5f729
Srđan <srdjan.prodanovic@gmail.com>
2018-07-02 19:42:25 +0200

    Fix paths

--- packages/multiplytix/src/index.js
+++ packages/multiplytix/src/index.js
@@ -1,11 +1,11 @@
 import GoogleAnalytics from './modules/google_analytics'
 import FacebookPixel from './modules/facebook_pixel'
 import Mixpanel from './modules/mixpanel'
-import GoogleRemarketingTag from 'modules/google_remarketing_tag'
-import Twitter from 'modules/twitter'
-import Pinterest from 'modules/pinterest'
-import Reddit from 'modules/reddit'
-import Hotjar from 'modules/hotjar'
+import GoogleRemarketingTag from './modules/google_remarketing_tag'
+import TwitterPixel from './modules/twitter_pixel'
+import PinterestPixel from './modules/pinterest_pixel'
+import RedditPixel from './modules/reddit_pixel'
+import Hotjar from './modules/hotjar'
 
 export const isServerSide = navigator.userAgent.includes('Node.js')
 export const isDevelopment = process.env.NODE_ENV === 'development'
@@ -39,21 +39,21 @@ class Multiplytix {
       google_analytics,
       google_remarketing,
       mixpanel,
+      hotjar,
       facebook_pixel,
-      twitter,
-      pinterest,
-      reddit,
-      hotjar
+      twitter_pixel,
+      pinterest_pixel,
+      reddit_pixel,
     } = config
 
     google_analytics && this.modules.push(new GoogleAnalytics(google_analytics))
     google_remarketing && this.modules.push(new GoogleRemarketingTag(google_remarketing))
-    facebook_pixel && this.modules.push(new FacebookPixel(facebook_pixel))
     mixpanel && this.modules.push(new Mixpanel(mixpanel))
-    twitter && this.modules.push(new Twitter(twitter))
-    reddit && this.modules.push(new Reddit(reddit))
-    pinterest && this.modules.push(new Pinterest(pinterest))
     hotjar && this.modules.push(new Hotjar(hotjar))
+    facebook_pixel && this.modules.push(new FacebookPixel(facebook_pixel))
+    twitter_pixel && this.modules.push(new TwitterPixel(twitter_pixel))
+    reddit_pixel && this.modules.push(new RedditPixel(reddit_pixel))
+    pinterest_pixel && this.modules.push(new PinterestPixel(pinterest_pixel))
 
     console.log('Multiplytix: Added', this.modules)
   }
