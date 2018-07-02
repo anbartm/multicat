dbfdc3aac7c3855e6cca70f03655b43aa26e3c8e
Srđan <srdjan.prodanovic@gmail.com>
2018-07-02 19:38:11 +0200

    Enable other providers

--- packages/multiplytix/src/index.js
+++ packages/multiplytix/src/index.js
@@ -1,14 +1,11 @@
-// import React, { PureComponent } from 'react'
-// import PropTypes from 'prop-types'
-
 import GoogleAnalytics from './modules/google_analytics'
 import FacebookPixel from './modules/facebook_pixel'
 import Mixpanel from './modules/mixpanel'
-// import GoogleRemarketingTag from 'modules/google_remarketing_tag'
-// import Twitter from 'modules/twitter'
-// import Pinterest from 'modules/pinterest'
-// import Reddit from 'modules/reddit'
-// import Hotjar from 'modules/hotjar'
+import GoogleRemarketingTag from 'modules/google_remarketing_tag'
+import Twitter from 'modules/twitter'
+import Pinterest from 'modules/pinterest'
+import Reddit from 'modules/reddit'
+import Hotjar from 'modules/hotjar'
 
 export const isServerSide = navigator.userAgent.includes('Node.js')
 export const isDevelopment = process.env.NODE_ENV === 'development'
@@ -29,7 +26,7 @@ class Multiplytix {
 
   view(event) {
     if (!isProduction || isServerSide) {
-      console.log('PageView', event)
+      console.log('PageViewEvent', event)
       return false
     }
 
@@ -40,13 +37,23 @@ class Multiplytix {
   initAnalytics(config) {
     const {
       google_analytics,
+      google_remarketing,
       mixpanel,
       facebook_pixel,
+      twitter,
+      pinterest,
+      reddit,
+      hotjar
     } = config
 
     google_analytics && this.modules.push(new GoogleAnalytics(google_analytics))
+    google_remarketing && this.modules.push(new GoogleRemarketingTag(google_remarketing))
     facebook_pixel && this.modules.push(new FacebookPixel(facebook_pixel))
     mixpanel && this.modules.push(new Mixpanel(mixpanel))
+    twitter && this.modules.push(new Twitter(twitter))
+    reddit && this.modules.push(new Reddit(reddit))
+    pinterest && this.modules.push(new Pinterest(pinterest))
+    hotjar && this.modules.push(new Hotjar(hotjar))
 
     console.log('Multiplytix: Added', this.modules)
   }
